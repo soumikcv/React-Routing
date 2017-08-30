57dabb40e0df7c8ed5dc666d70ead0f8969b08f1
soumikcv <soumiksur.me@gmail.com>
2017-08-31 01:44:23 +0530

    initialized

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/app';
+
+
+
+export default (
+	<Route path="/" component={App} />
+);
